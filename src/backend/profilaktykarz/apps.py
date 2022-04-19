4d8df1d6d559b94de22e8586f782220926f2779c
Joanna <joanna.bieganowska@gmail.com>
2022-04-19 17:42:37 +0200

    Added Flask React template

--- src/backend/profilaktykarz/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfilaktykarzConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.profilaktykarz'
--- web/src/i18n/pl.ts
+++
@@ -1,3 +0,0 @@
-const pl = {}
-
-export default pl
