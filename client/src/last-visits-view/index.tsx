02c21fa2fec3cc5468fc639d91b458a44e7a8b26
Adam Jędrzejec <adamjedrzejec@gmail.com>
2022-06-05 16:20:43 +0200

    [add] submit button

--- client/src/last-visits-view/index.tsx
+++ client/src/last-visits-view/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import {Box, Container, Stack, useToast} from '@chakra-ui/react'
+import {Button, Container, Stack, useToast} from '@chakra-ui/react'
 
 import {getApiLastVisits} from '@/api'
 import {LastVisitCheckup} from '@/types'
+
 import CheckupItem from './checkup-item'
 
 const LastVisitsView = () => {
@@ -35,10 +36,11 @@ const LastVisitsView = () => {
 
   return (
     <Container maxW="container.md">
-      <Stack spacing="5">
+      <Stack spacing="5" py="5">
         {checkups.map((checkup, i) => (
           <CheckupItem key={i} item={checkup} />
         ))}
+        <Button colorScheme="green">Wyślij</Button>
       </Stack>
     </Container>
   )
